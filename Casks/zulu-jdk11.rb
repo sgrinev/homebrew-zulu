174c6d991bf87068e9bcf0c3c99a1e735ea56545
Sergey Grinev <sgrinev@SergiusMBP18.local>
2019-08-26 16:18:23 +0300

    pkg sha fix

--- Casks/zulu-jdk11.rb
+++ Casks/zulu-jdk11.rb
@@ -1,6 +1,6 @@
 cask 'zulu-jdk11' do
   version '11.0.4,11'
-  sha256 '48c07dcce8be5e8ac5a4d0a602f96940883af235a7931a4b3b6ee1cb85c07d95'
+  sha256 '92a16dc44edb1df78f37ea0334a6b1efc49321f8e79fca66f029d00b1818e922'
 
   url "http://cherry.azulsystems.com/evaluation_builds/zulu11.33.15_1-ca-jdk11.0.4-macosx_x64.pkg"
   name 'Azul Zulu JDK 11'
